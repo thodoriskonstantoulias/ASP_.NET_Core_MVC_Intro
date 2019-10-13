775d718be3d8353458669d76576737839fbfa4d8
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-13 22:12:34 +0300

    Addition of EF Core, seed data

--- PieShop/Models/Pie.cs
+++ PieShop/Models/Pie.cs
@@ -11,7 +11,7 @@ namespace PieShop.Models
         public string Name { get; set; }
         public string ShortDescription { get; set; }
         public string LongDescription { get; set; }
-        public string AllergyInformation { get; set; }
+        public string AllergyInformation { get; set; } 
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
         public string ImageThumbnailUrl { get; set; }
@@ -19,5 +19,6 @@ namespace PieShop.Models
         public bool InStock { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+        public string Notes { get; set; }
     }
 }
