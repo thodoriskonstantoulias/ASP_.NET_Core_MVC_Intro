886f0590e140bfab7d59c42cdba2330f04f451dd
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-14 21:35:15 +0300

    Cretated Add to cart functionality and views, and table for cart

---
+++ PieShop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
+
+namespace PieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.GetPiesOfTheWeek()
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
