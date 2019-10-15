e51832f4600284fe4be7a2ba7039e76090c6f6ae
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-15 21:35:53 +0300

    Added order form and validation

--- PieShop/Models/AppDbContext.cs
+++ PieShop/Models/AppDbContext.cs
@@ -14,6 +14,8 @@ namespace PieShop.Models
         public DbSet<Pie> Pies { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
